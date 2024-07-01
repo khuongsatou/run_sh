624802c4a06187c15ea549a3cedf0423bbe9274d
Apple <apple@Apples-MacBook-Pro.local>
2024-07-01 13:56:17 +0700

    U

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Tạo file note
+echo "chmod +x run.sh&./run.sh" > readme.md
+
+# Tạo file requirements.txt và thêm các gói cần thiết
+echo "flask" > requirements.txt
+echo "requests" >> requirements.txt
+echo "numpy" >> requirements.txt
+echo "pandas" >> requirements.txt
+
+# Tạo môi trường ảo
+python3 -m venv env
+
+# Kích hoạt môi trường ảo
+source env/bin/activate
+
+# Cài đặt các gói từ file requirements.txt
+pip install -r requirements.txt
+
